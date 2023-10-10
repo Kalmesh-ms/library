188d5e7cf535bb2b875382612b48293fa3ae2739
Kalmesh-ms <141908865+Kalmesh-ms@users.noreply.github.com>
2023-10-10 11:12:50 +0530

    consoleLog Library with Test_book

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 const myLibrary = [];
+
+function Book(title , author , pages , status){
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.status = status
+}
+
+function addBookToLibrary(newBook){
+    myLibrary.push(newBook)
+}
+
+const harryPotter = new Book("Harry Potter" , "J K Rowling" , 123 , "read")
+
+addBookToLibrary(harryPotter)
