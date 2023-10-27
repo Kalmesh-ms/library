328dec4f6166f620c6fdbd007bde1f5319bb89f2
Kalmesh-ms <141908865+Kalmesh-ms@users.noreply.github.com>
2023-10-27 16:28:57 +0530

    display & add book to library & styleAddBtn

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 const myLibrary = [];
 
-const div = document.createElement('div')
+const bookDiv = document.createElement('div')
+const bookContainer = document.querySelector('.grid-container')
 
-function Book(title, author, pages, read ){
+function Book(title, author, pages, read  ){
     this.title = title,
     this.author = author,
     this.pages = pages,
-    this.isRead = false
+    this.read = read
 }
 
 function addBookToLibrary(bookName){
     myLibrary.push(bookName)
 }
 
+function displayBook(books){
+    bookDiv.textContent = `${books.title}, 
+    ${books.author}, ${books.pages},  ${books.read}`
+    bookDiv.classList.add('book')
+    bookContainer.appendChild(bookDiv)
+}
 
 
-harryPotter = new Book ('Harry Potter', 'J K Rowling', 230 , 'read')
+
+harryPotter = new Book ('Harry Potter', 'J K Rowling', 230 , 'Read')
+atomicHabits = new Book ('Atomic Habits',  'James Clear', 234 , "Read")
 
 addBookToLibrary(harryPotter)
-
+displayBook(harryPotter)
+addBookToLibrary(atomicHabits)
+displayBook(atomicHabits)
